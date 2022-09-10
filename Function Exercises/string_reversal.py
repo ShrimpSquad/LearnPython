ff88b0ef889260b25b4a6550813d3d2c0e788858
sebbyolive <97519992+sebbyolive@users.noreply.github.com>
2022-09-10 19:23:16 +0200

    Create string_reversal.py

---
+++ Function Exercises/string_reversal.py
@@ -0,0 +1,7 @@
+# Create a function that reverses a given string
+# e.g., abcdefg becomes gfedcba
+
+def str_reverse(string):
+    return string[::-1]
+
+print(str_reverse("Hello I am a backwards string!"))
